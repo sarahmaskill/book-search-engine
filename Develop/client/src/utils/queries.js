6aa69c75eeb0cdc08e8998546b01e9d80f279ff2
Sarah Maskill <sarahnmaskill@gmail.com>
2022-01-16 13:21:30 -0500

    working on client side

---
+++ Develop/client/src/utils/queries.js
@@ -0,0 +1,31 @@
+import { gql } from '@apollo/client';
+
+export const QUERY_USER = gql`
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      username
+      email
+    }
+  }
+`;
+
+import gql from 'graphql-tag';
+
+export const QUERY_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+      }
+    }
+  }
+`;
